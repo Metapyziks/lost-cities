d321a4e198c41a586a330a2497cd4f896394f23e
James King <james.king@facepunchstudios.com>
2023-12-21 17:01:48 +0100

    Add example .bat to run games

---
+++ greedy-vs-random.bat
@@ -0,0 +1,10 @@
+@echo off
+
+if not exist Results mkdir Results
+
+LostCities.Game\bin\Release\net7.0\LostCities.Game.exe ^
+    --player1 Examples\bin\Release\net7.0\Greedy.exe ^
+    --player2 Examples\bin\Release\net7.0\Random.exe ^
+    --games 100 ^
+    --parallel 8 ^
+    --output Results\greedy-vs-random-100.json
