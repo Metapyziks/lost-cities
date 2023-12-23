3e010360a7516fbd2e9e72d5edf409a4e83e9d2f
James King <james.king@facepunchstudios.com>
2023-12-23 15:04:44 +0100

    Support arguments for player commands

--- LostCities.Game/Program.cs
+++ LostCities.Game/Program.cs
@@ -1,70 +1,140 @@
 ﻿using System.Collections.Concurrent;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace LostCities.Game;
 
 public class Program
 {
-    /// <summary>
-    /// Run one or more games with the given pair of players.
-    /// </summary>
-    /// <param name="player1">Command to run for player 1. If null, will use standard input / output.</param>
-    /// <param name="player2">Command to run for player 2. If null, will use standard input / output.</param>
-    /// <param name="output">File path to write game results.</param>
-    /// <param name="gameSeed">Seed to use when shuffling the deck.</param>
-    /// <param name="player1Seed">Seed to pass to player 1.</param>
-    /// <param name="player2Seed">Seed to pass to player 2.</param>
-    /// <param name="games">How many games to play in a row.</param>
-    /// <param name="parallel">How many games to run simultaneously.</param>
-    /// <param name="fullResults">If true, output full results to file.</param>
-    /// <param name="human">If true, standard input / output players will have a human-readable interface.</param>
-    public static async Task<int> Main( FileInfo? player1 = null, FileInfo? player2 = null, FileInfo? output = null,
-        int? gameSeed = null, int? player1Seed = null, int? player2Seed = null,
-        int games = 1, int parallel = 1, bool fullResults = false, bool human = false )
+    public static async Task<int> Main( string[] args )
     {
-        var configs = Enumerable.Range( 0, games )
-            .Select( i => new GameConfig(
-                gameSeed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ),
-                player1Seed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ),
-                player2Seed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ) ) )
+        var player1Option = new Option<string[]>(
+            name: "--player1",
+            description: "Command to run for player 1. If omitted, will use standard input / output.",
+            getDefaultValue: Array.Empty<string> )
+        {
+            AllowMultipleArgumentsPerToken = true
+        };
+
+        var player2Option = new Option<string[]>(
+            name: "--player2",
+            description: "Command to run for player 2. If omitted, will use standard input / output.",
+            getDefaultValue: Array.Empty<string> )
+        {
+            AllowMultipleArgumentsPerToken = true
+        };
+
+        var outputOption = new Option<FileInfo?>(
+            name: "--output",
+            description: "File path to write game results.",
+            getDefaultValue: () => null );
+
+        var gameSeedOption = new Option<int?>(
+            name: "--game-seed",
+            description: "Seed to use when shuffling the deck. Random by default.",
+            getDefaultValue: () => null );
+
+        var player1SeedOption = new Option<int?>(
+            name: "--player1-seed",
+            description: "Seed to pass to player 1. Random by default.",
+            getDefaultValue: () => null );
+
+        var player2SeedOption = new Option<int?>(
+            name: "--player2-seed",
+            description: "Seed to pass to player 2. Random by default.",
+            getDefaultValue: () => null );
+
+        var gameCountOption = new Option<int>(
+            name: "--games",
+            description: "How many games to play in a row.",
+            getDefaultValue: () => 1 );
+
+        var maxParallelCountOption = new Option<int>(
+            name: "--max-parallel",
+            description: "How many games to run simultaneously.",
+            getDefaultValue: () => 1 );
+
+        var fullResultsOption = new Option<bool>(
+            name: "--full-results",
+            description: "If true, output full results to file.",
+            getDefaultValue: () => false );
+
+        var humanPlayerOption = new Option<bool>(
+            name: "--human",
+            description: "If true, standard input / output players will have a human-readable interface.",
+            getDefaultValue: () => false );
+
+        var rootCommand = new RootCommand( "Run one or more games with the given pair of players." )
+        {
+            player1Option, player2Option, outputOption,
+            gameSeedOption, player1SeedOption, player2SeedOption,
+            gameCountOption, maxParallelCountOption,
+            fullResultsOption, humanPlayerOption
+        };
+
+        rootCommand.SetHandler( async ( context ) =>
+        {
+            var player1Cmd = context.ParseResult.GetValueForOption( player1Option )!;
+            var player2Cmd = context.ParseResult.GetValueForOption( player2Option )!;
+            var outputFile = context.ParseResult.GetValueForOption( outputOption );
+            var gameSeed = context.ParseResult.GetValueForOption( gameSeedOption );
+            var player1Seed = context.ParseResult.GetValueForOption( player1SeedOption );
+            var player2Seed = context.ParseResult.GetValueForOption( player2SeedOption );
+            var gameCount = context.ParseResult.GetValueForOption( gameCountOption );
+            var maxParallelCount = context.ParseResult.GetValueForOption( maxParallelCountOption );
+            var fullResults = context.ParseResult.GetValueForOption( fullResultsOption );
+            var humanPlayer = context.ParseResult.GetValueForOption( humanPlayerOption );
+
+            var configs = Enumerable.Range( 0, gameCount )
+                .Select( i => new GameConfig(
+                    gameSeed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ),
+                    player1Seed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ),
+                    player2Seed ?? RandomNumberGenerator.GetInt32( int.MinValue, int.MaxValue ) ) )
             .ToArray();
 
-        if ( parallel > 1 && (player1 == null || player2 == null) )
-        {
-            throw new ArgumentException( "Can't run multiple games simultaneously over standard input." );
-        }
+            if ( maxParallelCount > 1 && (player1Cmd.Length == 0 || player2Cmd.Length == 0) )
+            {
+                throw new ArgumentException( "Can't run multiple games simultaneously over standard input." );
+            }
 
-        var task = parallel > 1
-            ? RunGamesParallelAsync( player1!.FullName, player2!.FullName, configs, parallel )
-            : RunGamesAsync( player1?.FullName, player2?.FullName, configs, human );
+            var task = maxParallelCount > 1
+                ? RunGamesParallelAsync( player1Cmd, player2Cmd, configs, maxParallelCount )
+                : RunGamesAsync( player1Cmd, player2Cmd, configs, humanPlayer );
 
-        var results = await task;
+            var results = await task;
 
-        if ( games == 1 )
-        {
-            PrintGameConfig( configs[0] );
-            PrintGameResult( results[0] );
-        }
+            if ( gameCount == 1 )
+            {
+                PrintGameConfig( configs[0] );
+                PrintGameResult( results[0] );
+            }
 
-        Console.WriteLine();
-        Console.WriteLine( "=== Final Results ===" );
-        Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine( "=== Final Results ===" );
+            Console.WriteLine();
 
-        PrintPlayerStats( results, Player.Player1 );
-        Console.WriteLine();
-        PrintPlayerStats( results, Player.Player2 );
-        Console.WriteLine();
+            PrintPlayerStats( results, Player.Player1 );
+            Console.WriteLine();
+            PrintPlayerStats( results, Player.Player2 );
+            Console.WriteLine();
 
-        if ( output != null )
-        {
-            var resultData = new GameResults( configs, results, fullResults );
-            await File.WriteAllTextAsync( output.FullName, resultData.ToJson() );
-        }
+            if ( outputFile != null )
+            {
+                var resultData = new GameResults( configs, results, fullResults );
+                await File.WriteAllTextAsync( outputFile.FullName, resultData.ToJson() );
+            }
+        } );
+
+        var result = rootCommand.Parse( args );
+
+        await result.InvokeAsync();
 
         return 0;
     }
 
-    private static async Task<IReadOnlyList<GameResult>> RunGamesAsync( string? player1Path, string? player2Path, IReadOnlyList<GameConfig> configs, bool human )
+    private static async Task<IReadOnlyList<GameResult>> RunGamesAsync( string[] player1Cmd, string[] player2Cmd, IReadOnlyList<GameConfig> configs, bool human )
     {
         var results = new List<GameResult>( configs.Count );
 
@@ -74,7 +144,7 @@ public class Program
         {
             Console.WriteLine( $"Game: {results.Count + 1}" );
 
-            var result = await RunGameAsync( player1Path, player2Path, config, human );
+            var result = await RunGameAsync( player1Cmd, player2Cmd, config, human );
             results.Add( result );
         }
 
@@ -84,7 +154,7 @@ public class Program
         return results;
     }
 
-    private static async Task<IReadOnlyList<GameResult>> RunGamesParallelAsync( string player1Path, string player2Path, IReadOnlyList<GameConfig> configs, int maxParallel )
+    private static async Task<IReadOnlyList<GameResult>> RunGamesParallelAsync( string[] player1Cmd, string[] player2Cmd, IReadOnlyList<GameConfig> configs, int maxParallel )
     {
         var results = new ConcurrentBag<(int Index, GameResult Result)>();
         var options = new ParallelOptions
@@ -94,7 +164,7 @@ public class Program
 
         var task = Parallel.ForEachAsync( configs.Select( (x, i) => (Index: i, Config: x) ), options, async ( x, _ ) =>
         {
-            var result = await RunGameAsync( player1Path, player2Path, x.Config, false );
+            var result = await RunGameAsync( player1Cmd, player2Cmd, x.Config, false );
             results.Add( (x.Index, result) );
         } );
 
@@ -126,17 +196,17 @@ public class Program
         return sortedResults;
     }
 
-    private static IPlayer CreatePlayer( string? path, Player player, bool human )
+    private static IPlayer CreatePlayer( string[] cmd, Player player, bool human )
     {
-        return string.IsNullOrEmpty( path )
+        return cmd.Length == 0
             ? human ? new HumanConsolePlayer( player ) : new ConsolePlayer( player )
-            : new ChildProcessPlayer( path );
+            : new ChildProcessPlayer( cmd[0], cmd.Skip( 1 ).ToArray() );
     }
 
-    private static async Task<GameResult> RunGameAsync( string? player1Path, string? player2Path, GameConfig config, bool human )
+    private static async Task<GameResult> RunGameAsync( string[] player1Cmd, string[] player2Cmd, GameConfig config, bool human )
     {
-        using var p1 = CreatePlayer( player1Path, Player.Player1, human );
-        using var p2 = CreatePlayer( player2Path, Player.Player2, human );
+        using var p1 = CreatePlayer( player1Cmd, Player.Player1, human );
+        using var p2 = CreatePlayer( player2Cmd, Player.Player2, human );
 
         return await LostCities.RunGameAsync( config, p1, p2 );
     }
